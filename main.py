1b4d3047eb5b1df708a9a7e4cca0d9e54b2709f8
Evaldas Dudonis <evaldasdudonis1@hotmail.lt>
2023-04-23 12:39:53 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,23 @@
+def return_names(names: list, number: int):
+    for i in range(0, len(names)):
+        for j in range(0, len(names) - i - 1):
+            if (names[j][1] > names[j + 1][1]):
+                temporary = names[j]
+                names[j] = names[j + 1]
+                names[j + 1] = temporary
+    return names[:number]
+
+listas = [
+    ['Jonas', 178],
+    ['Petras', 201],
+    ['Maryte', 168],
+    ['Gediminas', 178],
+    ['Vytautas', 183],
+]
+
+
+print(return_names(listas, 3))
+# listas.sort()
+# print(listas)
+# a = sorted(listas)
+# print(a)
