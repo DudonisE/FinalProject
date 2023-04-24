b0d7edf19b8d8fb9e2dc7501d9b72c3e5cc6a207
Evaldas Dudonis <evaldasdudonis1@hotmail.lt>
2023-04-24 22:00:41 +0300

    Updated user models and created form Updated requirements

--- user/forms.py
+++ user/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from user.models import BodyMeasurements
 
 
 class RegisterForm(UserCreationForm):
@@ -9,3 +10,9 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
+
+
+class BodyMeasurementsForm(forms.ModelForm):
+    class Meta:
+        model = BodyMeasurements
+        exclude = ('last_updated',)
