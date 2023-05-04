9ad59ec07b7880602ae7fd7d9d6698c7fc1020c8
ktoloc <122923482+ktoloc@users.noreply.github.com>
2023-05-04 21:27:17 +0300

    profile update

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from store.models import Size, Category, Product
+
+
+admin.site.register(Size)
+admin.site.register(Category)
+admin.site.register(Product)
