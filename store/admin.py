a2e14e67241d6a171d1b8fa55fb60ffb3f44f12d
Evaldas Dudonis <evaldasdudonis1@hotmail.lt>
2023-06-20 23:48:09 +0300

    Minor fixes in store. Started models of services creation

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from store.models import Size, Category, Product
+from store.models import Size, Category, Product, Order, CartItem
 
 
 admin.site.register(Size)
 admin.site.register(Category)
 admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(CartItem)
