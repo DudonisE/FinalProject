30edc6690d93492921bddcaff40e435867f66439
ktoloc <122923482+ktoloc@users.noreply.github.com>
2023-06-27 22:18:01 +0300

    store update 3

--- store/urls.py
+++ store/urls.py
@@ -4,11 +4,9 @@ from store import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('products/<int:pk>/', views.ProductDetail.as_view()),
-    path('women_products/<int:pk>/', views.one_product, name='product-view'),
-    path('women_products/', views.women_products, name='women-products-view'),
-    path('men_products/', views.men_products, name='men-products-view'),
-    path('women_products/<str:category_name>/', views.category_list, name='categorys'),
+    path('<str:gender>/', views.products_by_gender, name='product_by_gender'),
+    path('<str:gender>/<str:category_name>/', views.products_by_category, name='product_by_category'),
+    path('<int:pk>/', views.one_product, name='product-view'),
     path('search', views.search_feature, name='search-view'),
     path('cart/', views.show_cart, name='show_cart'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,3 +1,5 @@
+from django.views import generic
+
 from store import cart
 from django.shortcuts import render, get_object_or_404, redirect
 from rest_framework import generics
@@ -14,8 +16,24 @@ from store.serializers import ProductSerializer
 
 
 def index(request):
-    all_products = Product.objects.all()
-    return render(request, "index.html", {'all_products': all_products})
+    return render(request, 'index.html')
+
+
+def products_by_gender(request, gender):
+    products = Product.objects.filter(category_name__gender=gender)
+    return render(request, 'store/products_by_gender.html', {'products': products, 'gender': gender})
+
+
+def products_by_category(request, gender, category_name):
+    products = Product.objects.filter(category_name__gender=gender, category_name__category_name=category_name)
+    return render(request, 'store/products_by_categorys.html', {'products': products, 'gender': gender, 'category_name': category_name})
+
+
+
+
+def one_product(request, gender, pk):
+    product = Product.objects.get(category_name__gender=gender, id=pk)
+    return render(request, 'store/product.html', {'product': product})
 
 
 def search_feature(request):
@@ -26,44 +44,53 @@ def search_feature(request):
     return render(request, 'store/search.html', {})
 
 
-def women_products(request):
-    context = {
-        'women_items': Product.objects.filter(category_name__gender='Woman'),
-        'women_category': Category.objects.filter(gender='Woman'),
-
-    }
-    return render(request, "store/women_products.html", context)
-
-
-def men_products(request):
-    context = {
-        'men_items': Product.objects.filter(category_name__gender='Man'),
-        'men_category': Category.objects.filter(gender='Man'),
-    }
-    return render(request, "store/men_products.html", context)
+# class Products(generic.ListView):
+#     model = Product
+#     context_object_name = 'all_products'
+#     template_name = 'index.html'
+#
+#     def products_by_gender(self, *args, **kwargs):
+#         category = Category.objects.get(gender=self.kwargs['gender'])
+#         product = Product.objects.filter(category=category)
+#         context = {
+#             'product_list': product,
+#             'category_title': category,
+#         }
+#         return render(self.request, "category.html", context)
 
 
-def category_list(request, category_name):
-    category = Category.objects.filter(gender='Woman')
-    woman_category = get_object_or_404(category, category_name=category_name)
-    context = {'products': Product.objects.filter(category_name=woman_category)}
-    return render(request, "store/products_by_categorys.html", context)
+# def products_by_gender(request, gender):
+#     products = Product.objects.filter(category_name__gender=gender)
+#     return render(request, "store/products_by_gender.html", {'products': products, 'gender': gender})
+
+    # categorys = Product.objects.filter(category_name=category_name)
+    # product = get_object_or_404(Product, id=id)
+# def men_products(request):
+#     context = {
+#         'men_items': Product.objects.filter(category_name__gender='Man'),
+#         'men_category': Category.objects.filter(gender='Man'),
+#     }
+#     return render(request, "store/men_products.html", context)
 
 
-def one_product(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    if request.method == 'POST':
-        form = CartForm(request, request.POST)
-        if form.is_valid():
-            request.form_data = form.cleaned_data
-            cart.add_item_to_cart(request)
-            return redirect('show_cart')
+# def category_list(request, category_name):
+#     category = Category.objects.filter(gender='Woman')
+#     woman_category = get_object_or_404(category, category_name=category_name)
+#     context = {'products': Product.objects.filter(category_name=woman_category)}
+#     return render(request, "store/products_by_gender.html", context)
 
-    form = CartForm(request, initial={'product_id': product.id})
-    return render(request, 'store/product.html', {
-        'product': product,
-        'form': form,
-    })
+
+# def one_product(request, gender, pk):
+#     product = get_object_or_404(Product, category_name__gender=gender, id=pk)
+    # if request.method == 'POST':
+    #     form = CartForm(request, request.POST)
+    #     if form.is_valid():
+    #         request.form_data = form.cleaned_data
+    #         cart.add_item_to_cart(request)
+    #         return redirect('show_cart')
+    #
+    # form = CartForm(request, initial={'product_id': product.id})
+    # return render(request, 'store/product.html', {'product': product})
 
     # one_product = get_object_or_404(Product, pk=pk)
     # return render(request, "store/product.html", {'one_product': one_product})
@@ -107,3 +134,16 @@ def api_root(request, format=None):
         'snippets': reverse('snippet-list', request=request, format=format)
     })
 
+# < form
+# action = "{% url 'product-view' %}"
+# method = "post" >
+# { % csrf_token %}
+# {{form}}
+# < input
+# type = "submit"
+# value = "Add to cart"
+#
+#
+# class ="btn btn-primary" >
+#
+# < / form >
